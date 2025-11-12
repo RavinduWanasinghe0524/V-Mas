3f938eba5e6112127c90e714b62c4888094e68ed
Ravindu Wanasinghe <ravinduwanasinghe97@gmail.com>
2025-11-12 23:15:19 +0530

    feat: first commit

---
+++ common.js
@@ -0,0 +1,10 @@
+function checkAge(age) {
+    if (age >= 18) {
+        console.log("You are an adult");
+    } else {
+        console.log("You are a minor");
+    }
+}
+
+checkAge(20);
+checkAge(15);
