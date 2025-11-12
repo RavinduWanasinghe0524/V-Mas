5acd41c9741027cba8841b853cc3c846759d5d29
Ravindu Wanasinghe <ravinduwanasinghe97@gmail.com>
2025-11-12 23:17:19 +0530

    feat:ravindu js

---
+++ ravindu.js
@@ -0,0 +1,9 @@
+// Simple JavaScript file
+console.log("Hello, World!");
+
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+const message = greet("Ravindu");
+console.log(message);             
