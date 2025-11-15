47dd1deb8da2eaae61c4feed8f9e17658f612fa5
wathminiayesha <wathminiaye.com>
2025-11-15 12:17:25 +0530

    feat: add my file

---
+++ wathmini.js
@@ -0,0 +1,10 @@
+// Simple JavaScript file
+console.log("Hello, World!");
+console.log("Hello, World! 2 ");
+
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+const message = greet("Wathmini");
+console.log(message);             
